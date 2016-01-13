62f1325c3d96d7992eacd5c72e281ead81c2bb22
Juuso Minkkilä <K1756@student.jamk.fi>
2016-01-13 14:46:56 +0200

    added 8

---
+++ kerta1/1-8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vnro, unro;
+            Console.Write("Anna luku ");
+            vnro = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= 1; i++)
+            {
+                Console.Write("Anna luku ");
+                unro = int.Parse(Console.ReadLine());
+                if (unro > vnro)
+                    vnro = unro;
+            }
+            Console.WriteLine(vnro);
+        }
+    }
+}
