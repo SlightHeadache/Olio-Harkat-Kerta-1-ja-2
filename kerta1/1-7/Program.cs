1f1346349fee677a4c816db110eac463b7f43e51
Juuso Minkkilä <K1756@student.jamk.fi>
2016-01-13 14:06:41 +0200

    Upping the 1st two assingments.

---
+++ kerta1/1-7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vuosi;
+            Console.Write("Anna vuosiluku: ");
+            bool cs = int.TryParse(Console.ReadLine(), out vuosi);
+            if (cs)
+            {
+                if (vuosi % 100 == 0)
+                {
+                    if (vuosi % 400 == 0)
+                        Console.WriteLine("Vuosi " + vuosi + " on karkausvuosi. ");
+                    else
+                        Console.WriteLine("Vuosi " + vuosi + " ei ole karkausvuosi. ");
+                }
+                else if (vuosi % 4 == 0)
+                    Console.WriteLine("Vuosi " + vuosi + " on karkausvuosi. ");
+                else
+                    Console.WriteLine("Vuosi " + vuosi + " ei ole karkausvuosi. ");
+            }
+        }
+    }
+}
---
+++ kerta1/kerta1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kerta1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //7.02 l/100km
+            //1.595 €/l
+            //kysyy matkan
+            Console.Write("Anna matka kilometreinä: ");
+            float matka = float.Parse(Console.ReadLine());
+            Console.Write("Bensaa kuluu " + (matka / 100) * 7.02 + " litraa, kustannus " + (matka / 100) * 7.02 * 1.595 + " euroa. ");
+        }
+    }
+}
