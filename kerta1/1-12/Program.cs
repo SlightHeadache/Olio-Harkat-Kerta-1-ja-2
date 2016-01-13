88e815d5dadc9c313bea2a30a5a25a30ccf9a791
Juuso Minkkilä <K1756@student.jamk.fi>
2016-01-13 15:36:14 +0200

    12 added.

---
+++ kerta1/1-12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] taul = new int[5];
+            for(int i = 4;i >= 0; i--)
+            {
+                Console.Write("Anna luku: ");
+                taul[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Luvut ovat ");
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.Write(taul[i]);
+                if (i == 4)
+                {
+                    Console.Write(".");
+                    Console.WriteLine();
+                }
+                else
+                    Console.Write(", ");
+            }
+        }
+    }
+}
